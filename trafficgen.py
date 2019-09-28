d0b4e6ce0f34d5b03774e69fcde16e00f3fa017b
Fernando Farias <fernnf@gmail.com>
2019-09-28 12:53:46 -0300

    new

--- trafficgen.py
+++ trafficgen.py
@@ -62,7 +62,7 @@ class GenTrafficThroughput(object):
 
         def make_pkt():
             pkt = []
-            for _ in range(0, 500):
+            for _ in range(0, 1000):
                 # f = Ether(src=RandMAC(c), dst=RandMAC(),type=0x8100) / Dot1Q(vlan=20) / IP(dst=RandIP(),
                 # src=RandIP(a)) / UDP(dport=80)
                 f = Ether(src=RandMAC(c), dst=RandMAC()) / IP(dst=RandIP(), src=RandIP(a))
